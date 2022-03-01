c2078a32300b6973e021f69395e991c9421eae8c
idada-lyy <75367054+idada-lyy@users.noreply.github.com>
2022-03-01 14:34:33 +0800

    Initial commit

---
+++ src/components/Loading.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+//import * as Loader from 'react-loader-spinner';
+
+export const Loading = () => {
+  return (
+    <div className="flex justify-center items-center">
+    </div>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  //purge: [],
+  darkMode: 'class',
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
